a550903bd069d1cf2450b068409c94056a4fe3c7
bryan sun <hitsjt@gmail.com>
2020-05-05 17:11:19 +0800

    ...

---
+++ js模块化历程 - 吕大豹 - 博客园_files/online_v3.php
@@ -0,0 +1,6 @@
+var node=document.getElementById('cnzz_stat_icon_'+5597968);
+if(node){
+    node.innerHTML="<a href=https://www.cnzz.com/stat/website.php?web_id=5597968 target=_blank>站长统计</a><a href=https://www.cnzz.com/stat/website.php?web_id=5597968 target=_blank> |  今日IP[33] | 今日PV[45] | 昨日IP[120] |  昨日PV[168] | <a href=https://www.cnzz.com/stat/website.php?web_id=5597968&method=online target=_blank>当前在线[0]</a>";
+}else{
+    document.write("<a href=https://www.cnzz.com/stat/website.php?web_id=5597968 target=_blank>站长统计</a><a href=https://www.cnzz.com/stat/website.php?web_id=5597968 target=_blank> |  今日IP[33] | 今日PV[45] | 昨日IP[120] |  昨日PV[168] | <a href=https://www.cnzz.com/stat/website.php?web_id=5597968&method=online target=_blank>当前在线[0]</a>");
+}
